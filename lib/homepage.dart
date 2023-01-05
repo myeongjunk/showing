74064342420ae02b12bc2384aac33b35a474349e
fanxy <imakafanxy@handong.ac.kr>
2023-01-05 18:10:44 +0900

    third

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -9,6 +9,7 @@ class MainPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
+        surfaceTintColor: Colors.white,
         leading: IconButton(
           icon: const Icon(Icons.photo_camera_outlined),
           onPressed: () {},
@@ -42,7 +43,6 @@ class MainPage extends StatelessWidget {
                     child: Container(
                         child: Image.asset(
                       'assets/images/hwamin.png',
-                      //fit: BoxFit.fill,
                     )),
                   ),
                   Padding(
@@ -59,7 +59,7 @@ class MainPage extends StatelessWidget {
                             style: Lora_Title_Large(),
                           ),
                         ),
-                        const SizedBox(height: 5),
+                        const SizedBox(height: 10),
                         SizedBox(
                           width: 71,
                           height: 20,
@@ -98,7 +98,6 @@ class MainPage extends StatelessWidget {
                     child: Container(
                         child: Image.asset(
                       'assets/images/ming.png',
-                      //fit: BoxFit.fill,
                     )),
                   ),
                   Padding(
@@ -107,24 +106,37 @@ class MainPage extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         const SizedBox(height: 18),
-                        Text(
-                          'Treeming Bird',
-                          style: Lora_Title_Large(),
+                        SizedBox(
+                          height: 28,
+                          width: double.infinity,
+                          child: Text(
+                            'Treeming Bird',
+                            style: Lora_Title_Large(),
+                          ),
                         ),
                         const SizedBox(height: 10),
-                        Text(
-                          '@ tmb_ming',
-                          style: Lora_Label_Large(),
+                        SizedBox(
+                          height: 20,
+                          child: Text(
+                            '@ tmb_ming',
+                            style: Lora_Label_Large(),
+                          ),
                         ),
-                        const SizedBox(height: 15),
-                        Text(
-                          '어두운 감성으로 저만의 색깔을 표현해요.',
-                          style: Noto_Body2_Large(),
+                        const SizedBox(height: 5),
+                        SizedBox(
+                          height: 20,
+                          child: Text(
+                            '어두운 감성으로 저만의 색깔을 표현해요.',
+                            style: Noto_Body2_Large(),
+                          ),
                         ),
-                        const SizedBox(height: 15),
-                        Text(
-                          '자연과 함께 사진을 많이 찍었어요.',
-                          style: Noto_Body2_Large(),
+                        const SizedBox(height: 5),
+                        SizedBox(
+                          height: 20,
+                          child: Text(
+                            '자연과 함께 사진을 많이 찍었어요.',
+                            style: Noto_Body2_Large(),
+                          ),
                         ),
                       ],
                     ),
@@ -138,7 +150,6 @@ class MainPage extends StatelessWidget {
                     child: Container(
                         child: Image.asset(
                       'assets/images/okio.png',
-                      //fit: BoxFit.fill,
                     )),
                   ),
                   Padding(
@@ -147,24 +158,36 @@ class MainPage extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         const SizedBox(height: 18),
-                        Text(
-                          'Kyong',
-                          style: Lora_Title_Large(),
+                        SizedBox(
+                          height: 28,
+                          child: Text(
+                            'Kyong',
+                            style: Lora_Title_Large(),
+                          ),
                         ),
-                        const SizedBox(height: 15),
-                        Text(
-                          '@ 0ki0h',
-                          style: Lora_Label_Large(),
+                        const SizedBox(height: 10),
+                        SizedBox(
+                          height: 20,
+                          child: Text(
+                            '@ 0ki0h',
+                            style: Lora_Label_Large(),
+                          ),
                         ),
-                        const SizedBox(height: 15),
-                        Text(
-                          '카페 가는 것을 좋아해요.',
-                          style: Noto_Body2_Large(),
+                        const SizedBox(height: 5),
+                        SizedBox(
+                          height: 20,
+                          child: Text(
+                            '카페 가는 것을 좋아해요.',
+                            style: Noto_Body2_Large(),
+                          ),
                         ),
-                        const SizedBox(height: 15),
-                        Text(
-                          '주로 우드한 인테리어 카페를 많이 가는 것 같아요',
-                          style: Noto_Body2_Large(),
+                        const SizedBox(height: 5),
+                        SizedBox(
+                          height: 20,
+                          child: Text(
+                            '주로 우드한 인테리어 카페를 많이 가는 것 같아요',
+                            style: Noto_Body2_Large(),
+                          ),
                         ),
                       ],
                     ),
@@ -178,7 +201,6 @@ class MainPage extends StatelessWidget {
                     child: Container(
                         child: Image.asset(
                       'assets/images/sullendin.png',
-                      //fit: BoxFit.fill,
                     )),
                   ),
                   Padding(
@@ -187,24 +209,36 @@ class MainPage extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         const SizedBox(height: 18),
-                        Text(
-                          'Catgirl',
-                          style: Lora_Title_Large(),
+                        SizedBox(
+                          height: 28,
+                          child: Text(
+                            'Catgirl',
+                            style: Lora_Title_Large(),
+                          ),
                         ),
-                        const SizedBox(height: 15),
-                        Text(
-                          '@ sullendin',
-                          style: Lora_Label_Large(),
+                        const SizedBox(height: 10),
+                        SizedBox(
+                          height: 20,
+                          child: Text(
+                            '@ sullendin',
+                            style: Lora_Label_Large(),
+                          ),
                         ),
-                        const SizedBox(height: 15),
-                        Text(
-                          '스타일링 하는 것이 재밌어요.',
-                          style: Noto_Body2_Large(),
+                        const SizedBox(height: 5),
+                        SizedBox(
+                          height: 20,
+                          child: Text(
+                            '스타일링 하는 것이 재밌어요.',
+                            style: Noto_Body2_Large(),
+                          ),
                         ),
-                        const SizedBox(height: 15),
-                        Text(
-                          '다양한 포즈를 취하고 저만의 느낌을 주로 표현해요.',
-                          style: Noto_Body2_Large(),
+                        const SizedBox(height: 5),
+                        SizedBox(
+                          height: 20,
+                          child: Text(
+                            '다양한 포즈를 취하고 저만의 느낌을 주로 표현해요.',
+                            style: Noto_Body2_Large(),
+                          ),
                         ),
                       ],
                     ),
@@ -218,7 +252,6 @@ class MainPage extends StatelessWidget {
                     child: Container(
                         child: Image.asset(
                       'assets/images/boy.png',
-                      //fit: BoxFit.fill,
                     )),
                   ),
                   Padding(
@@ -227,24 +260,36 @@ class MainPage extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         const SizedBox(height: 18),
-                        Text(
-                          'Dajung',
-                          style: Lora_Title_Large(),
+                        SizedBox(
+                          height: 28,
+                          child: Text(
+                            'Dajung',
+                            style: Lora_Title_Large(),
+                          ),
                         ),
-                        const SizedBox(height: 15),
-                        Text(
-                          '@ awanderingboy',
-                          style: Lora_Label_Large(),
+                        const SizedBox(height: 10),
+                        SizedBox(
+                          height: 20,
+                          child: Text(
+                            '@ awanderingboy',
+                            style: Lora_Label_Large(),
+                          ),
                         ),
-                        const SizedBox(height: 15),
-                        Text(
-                          '쇼핑몰 운영하면서 여러 포즈와 보정을 많이 경험했어요.',
-                          style: Noto_Body2_Large(),
+                        const SizedBox(height: 5),
+                        SizedBox(
+                          height: 20,
+                          child: Text(
+                            '쇼핑몰 운영하면서 여러 포즈와 보정을 많이 경험했어요.',
+                            style: Noto_Body2_Large(),
+                          ),
                         ),
-                        const SizedBox(height: 15),
-                        Text(
-                          '스타일링에 따른 보정 분위기도 저의 색으로 나타냈어요.',
-                          style: Noto_Body2_Large(),
+                        const SizedBox(height: 5),
+                        SizedBox(
+                          height: 20,
+                          child: Text(
+                            '스타일링에 따른 보정 분위기도 저의 색으로 나타냈어요.',
+                            style: Noto_Body2_Large(),
+                          ),
                         ),
                       ],
                     ),
